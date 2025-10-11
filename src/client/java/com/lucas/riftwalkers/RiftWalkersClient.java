06bc8131079430791be3e0ee3e1a9125a28d73e3
Cooldude1259 <lucas.chrisopoulos@gmail.com>
2025-10-11 16:45:16 +1030

    Initial commit

---
+++ src/client/java/com/lucas/riftwalkers/RiftWalkersClient.java
@@ -0,0 +1,10 @@
+package com.lucas.riftwalkers;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RiftWalkersClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/lucas/riftwalkers/RiftWalkersDataGenerator.java
@@ -0,0 +1,11 @@
+package com.lucas.riftwalkers;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RiftWalkersDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
